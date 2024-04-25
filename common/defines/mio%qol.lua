804d2e8a4489e42afc084d3a8eeeb6e3f3933b6f
DeversusOrg <101480105+DeversusOrg@users.noreply.github.com>
2024-04-25 19:00:00 +0300

    delete it

--- common/defines/mio%qol.lua
+++
@@ -1,7 +0,0 @@
-NDefines.NIndustrialOrganisation.ASSIGN_DESIGN_TEAM_PP_COST_PER_DAY = 0					-- Cost in Political Power daily generation when one MIO is assigned to a research slot FREE
-NDefines.NIndustrialOrganisation.MAX_FUNDS_FROM_MANUFACTURER_PER_DAY = 500
-NDefines.NIndustrialOrganisation.DESIGN_TEAM_RESEARCH_BONUS = 0.15
-NDefines.NIndustrialOrganisation.DESIGN_TEAM_CHANGE_XP_COST = 1
-NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_BASE = 100
-NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_TRASHHOLD = 0
-NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_PENALTY = 0
