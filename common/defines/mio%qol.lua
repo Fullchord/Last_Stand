0a148ab69ace16b2014868e2ace6499549990ac3
DeversusOrg <101480105+DeversusOrg@users.noreply.github.com>
2024-04-26 19:47:23 +0300

    Create mio%qol.lua

---
+++ common/defines/mio%qol.lua
@@ -0,0 +1,9 @@
+NDefines.NIndustrialOrganisation.ASSIGN_DESIGN_TEAM_PP_COST_PER_DAY = 0					-- Cost in Political Power daily generation when one MIO is assigned to a research slot FREE
+NDefines.NIndustrialOrganisation.MAX_FUNDS_FROM_MANUFACTURER_PER_DAY = 500
+NDefines.NIndustrialOrganisation.DESIGN_TEAM_RESEARCH_BONUS = 0.15
+NDefines.NIndustrialOrganisation.DESIGN_TEAM_CHANGE_XP_COST = 1
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_BASE = 100
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_TRASHHOLD = 0
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_PENALTY = 0
+NDefines.NIntel.ARMY_ARMY_COUNT_RANGE_INTEL_MIN = 0.0 -- vilkata defines, removes invis divisions
+NDefines.NAir.SUPPLY_NEED_FACTOR = 0.01 -- vilkata defines, free supply for air, no trains needed
